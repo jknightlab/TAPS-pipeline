f5e9be90571096830bd9ad84a9c3b0dca55562e7
Eddie Cano Gamez <awo868@rescomp1.hpc.in.bmrc.ox.ac.uk>
2024-01-31 12:48:18 +0000

    Updating mbias argument format

--- 4_mbias-plot.sh
+++ 4_mbias-plot.sh
@@ -1,14 +1,74 @@
 #!/usr/bin/bash
 
-## Author:      Eddie Cano-Gamez (ecg@well.ox.ac.uk)
+## Author:	Kiki Cano-Gamez (kiki.canogamez@well.ox.ac.uk)
 
 ##########################################################################################
 # Specifying Slurm parameters for job submission
-#SBATCH -o /well/jknight/users/awo868/logs/TAPS-pipeline/mbias_%j.out 
+#SBATCH -A jknight.prj 
+#SBATCH -J mrkdup
 
-#SBATCH -e /well/jknight/users/awo868/logs/TAPS-pipeline/mbias_%j.err 
+#SBATCH -o /well/jknight/users/awo868/logs/TAPS-pipeline/sort-and-markdup_%j.out 
+
+#SBATCH -e /well/jknight/users/awo868/logs/TAPS-pipeline/sort-and-markdup_%j.err 
 #SBATCH -p long 
-#SBATCH -c 8
+#SBATCH -c 6
+##########################################################################################
+
+# Setting default parameter values
+input_dir=$PWD
+output_dir=$PWD
+referenceGenome='/well/jknight/projects/sepsis-immunomics/cfDNA-methylation/cfDNA-methylation_04-2023/results/TAPS-pipeline/methyl-dackel/reference-genome/GRCh38-reference_with-spike-in-sequences.fasta.gz'
+
+# Reading in arguments
+while getopts r:w:s:o:g:h opt
+do
+	case $opt in
+	i)
+		input_dir=$OPTARG
+		;;
+	o)
+		output_dir=$OPTARG
+		;;
+	s)
+		sample_list_path=$OPTARG
+		;;
+	g)
+		reference_genome=$OPTARG
+		;;
+	h)
+		echo "Usage:	mbias-plot.sh [-i input_dir] [-o output_dir] [-s sample_list_path] [-g reference_genome_path]"
+		echo ""
+		echo "Where:"
+		echo "-i		Path to input directory containing sorted BAM files for for methylation bias estimation [defaults to the working directory]"
+		echo "-o		Path to output directory where to store methylation bias plots [defaults to the working directory]"
+		echo "-s		Path to a text file containing a list of samples (one sample per line). Sample names should match file naming patterns."
+		echo "-g		Path to the reference genome file to be used (in FASTA format) [defaults to the human GRCh38 reference genome with added TAPS-specific spike-in sequences]"
+		echo ""
+		exit 1
+		;;
+	esac
+done
+
+# Validating arguments
+echo "[mbias-plot]:	Validating arguments..."
+
+if [[ ! -d $input_dir ]]
+then
+		echo "[mbias-plot]:	ERROR: Input directory not found."
+		exit 2
+fi 
+
+if [[ ! -d $output_dir ]]
+then
+		echo "[mbias-plot]:	ERROR: Output directory not found."
+        exit 2
+fi 
+
+if [[ ! -f $sample_list_path ]]
+then
+        echo "[mbias-plot]:	ERROR: Sample list file not found"
+        exit 2
+fi
 
 # Outputing relevant information on how the job was run
 echo "------------------------------------------------" 
@@ -19,30 +79,7 @@ echo "Started at: "`date`
 echo "Executing task ${SLURM_ARRAY_TASK_ID} of job ${SLURM_ARRAY_JOB_ID} "
 echo "------------------------------------------------" 
 
-
-##########################################################################################
-# General processing
-## Reading in arguments
-echo "===== TAPS pipeline step 4: Checking for methylation bias with MethylDackel ====="
-echo ""
-echo "[mbias-plot]:	Validating arguments..."
-if [[ ! -f $1 ]]
-then
-	echo "[mbias-plot]:	ERROR: Sample list file not found."
-	exit 2
-fi
-
-if [[ ! -d $2 ]]
-then
-	echo "[mbias-plot]:	ERROR: Output directory not found."
-	exit 2
-fi 
-
-echo "[mbias-plot]:	Reading sample list..."
-readarray sampleList < $1
-outDir=$2
-
-## Loading required modules and virtual environments
+# Loading required modules
 echo "[mbias-plot]:	Loading required modules..."
 module load Anaconda3/2022.05
 
@@ -50,18 +87,18 @@ echo "[mbias-plot]:	Loading virtual environment..."
 eval "$(conda shell.bash hook)"
 conda activate methylDackel
 
-## Defining path variables
-referenceGenome='/well/jknight/projects/sepsis-immunomics/cfDNA-methylation/cfDNA-methylation_04-2023/results/TAPS-pipeline/methyl-dackel/reference-genome/GRCh38-reference_with-spike-in-sequences.fasta.gz'
+# Parsing input file
+echo "[mbias-plot]:	Reading sample list..."
+readarray sampleList < $sample_list_path
 
-# Per-task processing 
-## Defining input and output names
+#  Parallelising process by sample
 sampleName=$(echo ${sampleList[$((${SLURM_ARRAY_TASK_ID}-1))]} | sed 's/\n//g')
 
-## Running MethylDackel
+# Running MethylDackel's methylation bias estimation function
 echo "[mbias-plot]:	Checking for methylation bias with MethylDackel ($sampleName)..."
 MethylDackel mbias \
 	$referenceGenome \
-	"${sampleName}.qced.sorted.markdup.bam" \
-	"${outDir}/${sampleName}"
+	"${input_dir}/${sampleName}.qced.sorted.markdup.bam" \
+	"${output_dir}/${sampleName}"
 
 echo "[mbias-plot]: ...done!"
