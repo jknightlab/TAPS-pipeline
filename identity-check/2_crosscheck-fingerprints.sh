1b1e4a5fda37759f3f807226a3a082bacb0e41c1
Eddie Cano Gamez <awo868@rescomp1.hpc.in.bmrc.ox.ac.uk>
2024-02-01 15:43:11 +0000

    Updating argument format

--- identity-check/2_crosscheck-fingerprints.sh
+++ identity-check/2_crosscheck-fingerprints.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-## Author:      Eddie Cano-Gamez (ecg@well.ox.ac.uk)
+## Author:	Kiki Cano-Gamez (kiki.canogamez@well.ox.ac.uk)
 
 ##########################################################################################
 # Specifying Slurm parameters for job submission
@@ -8,10 +8,59 @@
 #SBATCH -J check-identity-2
 
 #SBATCH -o /well/jknight/users/awo868/logs/TAPS-pipeline/check-identity-2_%j.out 
-#SBATCH -e /well/jknight/users/awo868/logs/TAPS-pipeline/check-identity-2_%j.err 
 
+#SBATCH -e /well/jknight/users/awo868/logs/TAPS-pipeline/check-identity-2_%j.err 
 #SBATCH -p short 
 #SBATCH -c 4
+##########################################################################################
+
+
+
+# Setting default parameter values
+input_dir=$PWD
+output_dir=$PWD
+fingerprint_map='/well/jknight/projects/sepsis-immunomics/cfDNA-methylation/cfDNA-methylation_04-2023/results/TAPS-pipeline/bwa-mem/reference-genome/hg38_chr.map'
+
+# Reading in arguments
+while getopts i:o:f:h opt
+do
+	case $opt in
+	i)
+		input_dir=$OPTARG
+		;;
+	o)
+		output_dir=$OPTARG
+		;;
+	f)
+		fingerprint_map=$OPTARG
+		;;
+	h)
+		echo "Usage:	add-read-groups.sh [-i input_dir] [-o output_dir] [-s sample_list_path] [-f fingerprint_map]"
+		echo ""
+		echo "Where:"
+		echo "-i		Path to input directory containing BAM files, previously read grouped [defaults to the working directory]"
+		echo "-o		Path to output directory where to write final LOD matrix [defaults to the working directory]"
+		echo "-f		Path to fingerprint map file for the reference genome used [defaults to a map file for GRCh38]"
+		echo ""
+		exit 1
+		;;
+	esac
+done
+
+## Validating arguments
+echo "[check-identity]:	Validating arguments..."
+
+if [[ ! -d $input_dir ]]
+then
+		echo "[check-identity]:	ERROR: Input directory not found."
+		exit 2
+fi 
+
+if [[ ! -d $output_dir ]]
+then
+		echo "[check-identity]:	ERROR: Output directory not found."
+        exit 2
+fi 
 
 # Outputing relevant information on how the job was run
 echo "------------------------------------------------" 
@@ -19,31 +68,8 @@ echo "Run on host: "`hostname`
 echo "Operating system: "`uname -s` 
 echo "Username: "`whoami` 
 echo "Started at: "`date` 
+echo "Executing task ${SLURM_ARRAY_TASK_ID} of job ${SLURM_ARRAY_JOB_ID} "
 echo "------------------------------------------------" 
-
-
-##########################################################################################
-
-
-# Reading in arguments
-echo "===== TAPS pipeline: Checking sample identity with Picard (2) ====="
-echo ""
-echo "[check-identity]:		Validating arguments..."
-if [[ ! -d $1 ]]
-then
-        echo "[check-identity]:		ERROR: Output directory not found."
-        exit 2
-fi
-
-if [[ ! -d $2 ]]
-then
-        echo "[check-identity]:		ERROR: Output directory not found."
-        exit 2
-fi 
-
-echo "[check-identity]:		Reading sample list..."
-inputDir=$1
-outputDir=$2
 
 # Loading required modules
 echo "[check-identity]:	Loading modules..."
@@ -52,31 +78,30 @@ module load picard/2.23.0-Java-11
 module load samtools/1.8-gcc5.4.0
 
 # Creating output directories
-if [[ ! -d "${outputDir}/tmp" ]]
+if [[ ! -d "${output_dir}/tmp" ]]
 then
-        mkdir "${outputDir}/tmp"
+        mkdir "${output_dir}/tmp"
 fi
 
 # Defining paths
-fingerprintMap='/well/jknight/projects/sepsis-immunomics/cfDNA-methylation/cfDNA-methylation_04-2023/results/TAPS-pipeline/bwa-mem/reference-genome/hg38_chr.map'
 
 # Merging BAM files
 echo "[check-identity]:		Merging BAM files..."	
-samtools merge -@ 12 ${outputDir}/tmp/merged_RG.bam ${inputDir}/*.qced.sorted.markdup.RG.bam 
+samtools merge -@ 12 "${output_dir}/tmp/merged_RG.bam" "${input_dir}/*.qced.sorted.markdup.RG.bam"
 
 echo "[check-identity]:		Cross-checking fingerprints..."	
 java -jar $EBROOTPICARD/picard.jar CrosscheckFingerprints \
-	INPUT="${outputDir}/tmp/merged_RG.bam" \
-	HAPLOTYPE_MAP="$fingerprintMap" \
+	INPUT="${output_dir}/tmp/merged_RG.bam" \
+	HAPLOTYPE_MAP="$fingerprint_map" \
 	NUM_THREADS=4 \
-	OUTPUT="${outputDir}/crosscheck-metrics.txt" \
-	MATRIX_OUTPUT="${outputDir}/crosscheck_LOD-matrix.txt"
+	OUTPUT="${output_dir}/crosscheck-metrics.txt" \
+	MATRIX_OUTPUT="${output_dir}/crosscheck_LOD-matrix.txt"
 
-if [[ -f "${outputDir}/crosscheck-metrics.txt" ]]
+if [[ -f "${output_dir}/crosscheck-metrics.txt" ]]
 then
 	echo "[check-identity]:		Cleaning up..."
-	rm ${inputDir}/*.qced.sorted.markdup.RG.bam
-	rm ${outputDir}/tmp/merged_RG.bam
+	rm "${input_dir}/*.qced.sorted.markdup.RG.bam"
+	rm "${output_dir}/tmp/merged_RG.bam"
 	
 	echo "[check-identity]:		...done!"	
 fi
